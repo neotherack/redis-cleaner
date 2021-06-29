544050d4c1303b50963de92f0e300c62e223b646
Fran Vallejo <francisco.vallejo-luna@microfocus.com>
2021-06-29 13:18:00 +0200

    [FEATURE] parameter handling improvements, ready to be configured as command crontab lines

--- redis_cleaner.py
+++ redis_cleaner.py
@@ -6,8 +6,9 @@ import click
 @click.option('--size', default=300, help='Size key limit [Kb] (default 300Kb)')
 @click.option('--delete', default=False, help='Delete if True, Report if False')
 def clean(days, size, delete):
-    scanned=0
-    dropped=0
+    scanned = 0
+    dropped = 0
+    mem_saved = 0
     drop_limit_days = days
     drop_limit_seconds = drop_limit_days * 24 * 60 * 60
 
@@ -15,16 +16,19 @@ def clean(days, size, delete):
     for key in r.scan_iter("*"):
         scanned = scanned+1
         idle = r.object("idletime", key)
+        idle_days = idle / 3600 / 24
         mem = r.memory_usage(key)
-        if idle >= drop_limit_seconds and mem>=size:
+        mem_kb = mem / 1024
+        if idle >= drop_limit_seconds and mem_kb>=size:
             if delete and delete==True:
-                print(f"Dropped key: {key}")
+                print(f"Dropped key: idle:{idle_days:.3f} mem:{mem_kb:.2f} - {key.decode('utf8')}")
                 dropped=dropped+1
+                mem_saved = mem_saved + mem_kb
                 r.delete(key)
             else:
                 print(f"To drop key: {key}")
 
-    print(f"Process completed, scanned {scanned} keys, dropped {dropped}")
+    print(f"Process completed, scanned {scanned} keys, dropped {dropped}. Saved {mem_saved} Kb")
 
 if __name__ == '__main__':
     clean()
--- redis_expirer.py
+++ redis_expirer.py
@@ -1,30 +1,39 @@
 import redis
+import click
 
-expire_days = 4
-expire_seconds = expire_days * 24 * 60 * 60
+@click.command()
+@click.option('--days', default=7, help='Idle time limit to filter (default 7)')
+@click.option('--size', default=300, help='Size key limit [Kb] (default 300Kb)')
+@click.option('--expire', default=False, help='Expire if True, Report if False')
+@click.option('--break_on', default=1000, help='Apply limit scan key (default 1000)')
+def expire_keys(days, size, expire, break_on):
+    expire_days = days
+    expire_seconds = expire_days * 24 * 60 * 60
 
-size_limit_kb = 100
-size_limit = size_limit_kb * 1024
-size_sum = 0
+    size_limit_kb = size
+    size_limit = size_limit_kb * 1024
+    size_sum=0
 
-break_on = None
+    print(f"Scanning keys not used in the last {expire_days} days, larger than {size_limit_kb} Kb")
+    print(f"ID;Size (kb);Idle (days);Key name")
+    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    for i, key in enumerate(r.scan_iter("*")):
+        ttl = r.ttl(key)
+        size = r.memory_usage(key)
+        if ttl == -1 and size >= size_limit:
+            idle = r.object("idletime", key)
+            size_sum = size_sum + size
+            if idle >= expire_seconds:
+               if expire==True:
+                   r.expire(key, expire_seconds)
+               else:
+                   print(f"{i};{(size / 1024):.2f};{(idle / 60 / 60 / 24):.2f};{key.decode('utf8')}")
 
-print(f"Scanning keys not used in the last {expire_days} days, larger than {size_limit_kb} Kb")
-print(f"ID;Size (kb);Idle (days);Key name")
+        if break_on and break_on <= i:
+            break
 
-r = redis.StrictRedis(host='localhost', port=6379, db=0)
-for i, key in enumerate(r.scan_iter("*")):
-    ttl = r.ttl(key)
-    size = r.memory_usage(key)
-    if ttl == -1 and size >= size_limit:
-        idle = r.object("idletime", key)
-        size_sum = size_sum + size
-        if idle >= expire_seconds:
-            print(f"{i};{(size / 1024):.2f};{(idle / 60 / 60 / 24):.2f};{key.decode('utf8')}")
-            #redis.expire(key, expire_seconds)
+    print(f"Process completed, analyzed {i} keys")
+    print(f"Memory excess {(size_sum / 1024):.2f} Kb | {(size_sum / 1024 / 1024):.2f} Mb")
 
-    if break_on and break_on <= i:
-        break
-
-print(f"Process completed, analyzed {i} keys")
-print(f"Memory excess {(size_sum / 1024):.2f} Kb | {(size_sum / 1024 / 1024):.2f} Mb")
+if __name__=="__main__":
+  expire_keys()
