551e8b516a58745a56a0ac3f797cef056cb0ffc4
Fran Vallejo <francisco.vallejo-luna@microfocus.com>
2021-06-29 11:48:54 +0200

    [FIX] minor fixes

--- redis_expirer.py
+++ redis_expirer.py
@@ -1,17 +1,17 @@
-#!/usr/bin/env python
 import redis
 
-expire_days = 7
+expire_days = 4
 expire_seconds = expire_days * 24 * 60 * 60
 
-size_limit_kb = 350
+size_limit_kb = 100
 size_limit = size_limit_kb * 1024
-
 size_sum = 0
 
-break_on = 1500
+break_on = None
 
-print(f"Scanning keys not used in the last {expire_days} days")
+print(f"Scanning keys not used in the last {expire_days} days, larger than {size_limit_kb} Kb")
+print(f"ID;Size (kb);Idle (days);Key name")
+
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 for i, key in enumerate(r.scan_iter("*")):
     ttl = r.ttl(key)
@@ -20,7 +20,7 @@ for i, key in enumerate(r.scan_iter("*")):
         idle = r.object("idletime", key)
         size_sum = size_sum + size
         if idle >= expire_seconds:
-            print(f"{(size / 1024):.2f} Kb IDLE for {(idle / 60 / 60 / 24):.2f} days @ {key.decode('utf8')}")
+            print(f"{i};{(size / 1024):.2f};{(idle / 60 / 60 / 24):.2f};{key.decode('utf8')}")
             #redis.expire(key, expire_seconds)
 
     if break_on and break_on <= i:
